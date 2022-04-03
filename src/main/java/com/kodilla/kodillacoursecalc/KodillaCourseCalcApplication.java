0f665fe92feb30f9e0a81a4b96d988bc5f9c59bd
Anna Burenok <humptydumpty@MacBook-Air-Anna.local>
2022-04-03 21:29:24 +0200

    Calculator

---
+++ src/main/java/com/kodilla/kodillacoursecalc/KodillaCourseCalcApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacoursecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/kodillacoursecalc/KodillaCourseCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacoursecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaCourseCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
