646b353d078792c60d25a14affa24b7b5518d091
humptydumpty <mateusz.wojtas89@gmail.com>
2022-04-25 13:31:32 +0200

    7.1 PoemBeautifier corrected

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,7 +2,6 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.Processor;
@@ -16,12 +15,9 @@ public class StreamMain {
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         String txt = "abc";
-        PoemDecorator toUpperCase = (String) -> txt.toUpperCase();
-        PoemDecorator toLoverCase = (String) -> txt.toLowerCase();
-        PoemDecorator replace = (String) -> txt.replace("b", "t");
-        poemBeautifier.beautify(txt, toUpperCase);
-        poemBeautifier.beautify(txt, toLoverCase);
-        poemBeautifier.beautify(txt, replace);
+        poemBeautifier.beautify(txt, (String) -> txt.toUpperCase());
+        poemBeautifier.beautify(txt, (String) -> txt.toLowerCase());
+        poemBeautifier.beautify(txt, (String) -> txt.replace("b", "t"));
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
