f01e4c1f91d55f0f8d764744bf27deecfc2ee389
humptydumpty <mateusz.wojtas89@gmail.com>
2022-04-26 15:59:35 +0200

    8.2 Try - Catch corrected

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,14 @@
 package com.kodilla.exception.main;
 
-import com.kodilla.exception.io.FileReader;
-
 public class ExceptionModuleRunner {
 
-    public static void main (String[] args) {
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
-    }
+//    public static void main (String[] args) {
+//        FileReader fileReader = new FileReader();
+//
+//        try {
+//            fileReader.readFile();
+//        } catch (FileReaderException e) {
+//            System.out.println("Problem while reading a file!");
+//        }
+//    }
 }
