5406b74f5be1eeb861b023353adc1904433d2801
humptydumpty <mateusz.wojtas89@gmail.com>
2022-05-27 16:17:39 +0200

    13.1 https://github.com/mwojtas89/mateusz-wojtas-calculator.git

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamMovies.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamMovies.java
@@ -6,15 +6,13 @@ public class StreamMovies {
 
     public static void main(String[] args) {
         MovieStore movies = new MovieStore();
+
         String print = movies.getMovies().entrySet().stream()
                 .flatMap(tl->tl.getValue().stream())
                 .map(a-> a = "!" + a)
                 .collect(Collectors.joining());
+
         print = print.replaceFirst("!","");
         System.out.println(print);
     }
-
-
-
-
 }
