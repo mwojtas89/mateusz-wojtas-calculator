1a34e01fb47ac802a605ee8f9d159dafd6348680
humptydumpty <mateusz.wojtas89@gmail.com>
2022-05-27 16:15:41 +0200

    13.1 Iteration through titles

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamMovies.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamMovies.java
@@ -1,21 +1,19 @@
 package com.kodilla.good.patterns.challenges;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamMovies {
 
     public static void main(String[] args) {
         MovieStore movies = new MovieStore();
-        List<String> print = movies.getMovies().entrySet().stream()
+        String print = movies.getMovies().entrySet().stream()
                 .flatMap(tl->tl.getValue().stream())
-                .collect(Collectors.toList());
-        String out = "";
-        for(String pr : print){
-            out = out + pr + "!";
-        }
-        System.out.println(out);
+                .map(a-> a = "!" + a)
+                .collect(Collectors.joining());
+        print = print.replaceFirst("!","");
+        System.out.println(print);
     }
+
 
 
 
